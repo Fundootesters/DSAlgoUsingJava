290e75f1978073985f2e0cd29fb5bda966c4591d
fundootesters <fundootesters@gmail.com>
2023-04-16 07:05:15 +0530

    bubble sort update

--- src/main/java/org/sorting/BubbleSort.java
+++ src/main/java/org/sorting/BubbleSort.java
@@ -1,6 +1,13 @@
 package org.sorting;
 
 public class BubbleSort {
+
+    //Origin: Initially, Bubble sort was referred to as “Sorting by exchange” in [1, 2] and further, it is
+    // re-ferred to as “Exchange Sorting” in [3, 4]. The term “Bubble Sort ” was first used by Iverson in 1962 [5].
+
+    // Understand via video
+    // In Hindi: https://www.youtube.com/watch?v=0IVVxxG3eOk&list=PLuqnSAN_m8K28CMJ1WglKviPxP-ax18DE
+    // In English: https://www.youtube.com/watch?v=udZtu7GIIhA&list=PLuqnSAN_m8K2GEvxrqEyR8aiPUHFdHF6Q
     private static int[]  sortArray(int[] inputArray) {
         for (int lastIndex = inputArray.length - 1; lastIndex > 0; lastIndex--) {
             for (int i = 0; i < lastIndex; i++) {
